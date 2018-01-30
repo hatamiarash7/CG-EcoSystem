f569c15a467055dce1261d0bbc03427870361a00
Arash Hatami <hatamiarash7@gmail.com>
2018-01-30 16:19:55 +0330

    add default prefix

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
+            'prefix' => 'cg_',
             'strict' => true,
             'engine' => null,
         ],
